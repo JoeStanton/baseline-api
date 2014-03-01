d6702a1012a5dcfb9d3882ad4da46208cb34efb1
Joe Stanton <joe.stanton@red-badger.com>
2014-03-01 18:40:37 +0000

    Standard graph #relate, #in, #out methods

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -38,3 +38,40 @@ describe Graph do
     end
   end
 end
+
+describe Node do
+  graph = Graph.new "relate"
+  n1 = Node.new(graph)
+  n2 = Node.new(graph)
+
+  graph.nodes << n1
+  graph.nodes << n2
+
+  describe "#relate" do
+    it "should create the relationship" do
+      n1.relate(n2, :depends_on)
+
+      edge = graph.edges.first
+      graph.edges.should have(1).item
+      edge.from.should == n1
+      edge.to.should == n2
+      edge.type.should == :depends_on
+    end
+
+    it "should not create duplicate relationships" do
+      n1.relate(n2, :depends_on)
+      graph.edges.should have(1).item
+    end
+  end
+
+  describe "#out" do
+    it "should list outgoing relationships" do
+      n1.out.should have(1).item
+    end
+
+    it "should constrain by type" do
+      n1.out(:depends_on).should have(1).item
+      n1.out(:nothing).should be_empty
+    end
+  end
+end
