e86a405e463992ae7065bf1aa63344e1a8923e14
Joe Stanton <joe.stanton@red-badger.com>
2014-03-01 18:40:37 +0000

    Standard graph #relate, #in, #out methods

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 class Graph
   attr_accessor :nodes, :edges
 
@@ -22,5 +24,37 @@ class Graph
 
   def save
     File.open(self.class.path(@name), 'w') {|f| f.write(Marshal.dump(self)) }
+  end
+end
+
+class Node
+  attr_accessor :id, :graph
+
+  def initialize(graph)
+    @id = graph.nodes.size + 1
+    @graph = graph
+  end
+
+  def relate(to, type)
+    return if out(type).map(&:to).include? to
+    graph.edges << OpenStruct.new(from: self, to: to, type: type)
+  end
+
+  def in(type = nil)
+    edges = graph.edges.select { |edge| edge.to }
+    if type
+      edges.select { |edge| edge.type == type }
+    else
+      edges
+    end
+  end
+
+  def out(type = nil)
+    edges = graph.edges.select { |edge| edge.from }
+    if type
+      edges.select { |edge| edge.type == type }
+    else
+      edges
+    end
   end
 end
