9684e0894c753f6b67fb8b054d048897dc144596
Joe Stanton <joe.stanton@red-badger.com>
2014-03-30 14:47:48 +0100

    Add discoverable API root

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,10 @@
 class ApiController < ApplicationController
   def index
-    render json: {}
+    render json: {
+      services: services_url,
+      hosts: hosts_url,
+      events: events_url,
+      incidents: incidents_url
+    }
   end
 end
