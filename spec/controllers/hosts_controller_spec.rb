8f25c874f951875d2e57abd365894f3cc34a52b2
Joe Stanton <joe.stanton@red-badger.com>
2014-04-12 19:18:05 +0100

    Added host registration specs

--- spec/controllers/hosts_controller_spec.rb
+++ spec/controllers/hosts_controller_spec.rb
@@ -26,15 +26,31 @@ describe HostsController do
   describe "PUT host status" do
     it "updates host status and triggers an incident" do
       host = @service.hosts.create(hostname: "problematic-host")
-      put :update, service_id: @service, id: "problematic-host", host: { status: "error" }
+      put :update, id: "problematic-host", host: { status: "error" }
       assert_response :success
 
       host.service.open_incident.should be_present
     end
 
+    it "supports registering a service" do
+      host = Host.create(hostname: "problematic-host")
+      put :update, id: "problematic-host", host: { service_slug: @service.slug }
+      assert_response :success
+
+      host.reload.service.slug.should == @service.slug
+    end
+
+    it "supports clearing the registered service" do
+      host = @service.hosts.create(hostname: "problematic-host")
+      put :update, id: "problematic-host", host: { service_slug: "nil" }
+      assert_response :success
+
+      host.reload.service.should be_nil
+    end
+
     it "updates host status and closes an incident" do
       host = @service.hosts.create(hostname: "problematic-host")
-      put :update, service_id: @service, id: "problematic-host", host: { status: "ok" }
+      put :update, service_slug: @service.slug, id: "problematic-host", host: { status: "ok" }
       assert_response :success
 
       host.service.open_incident.should_not be_present
