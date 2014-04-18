2829c309acd42014e7ce46da635f14972a432650
Joe Stanton <joe.stanton@red-badger.com>
2014-04-18 10:38:09 +0100

    Tweaked email templates and hrefs

--- spec/mailers/preview/incident_preview.rb
+++ spec/mailers/preview/incident_preview.rb
@@ -1,11 +1,15 @@
 class IncidentPreview < ActionMailer::Preview
   def detected
     incident = Incident.new(id: 101, service: Service.first, created_at: DateTime.now)
+    incident.components.build(name: "Nginx")
+    incident.hosts.new(hostname: "rb-prod-01")
     IncidentMailer.detected(incident)
   end
 
   def resolved
     incident = Incident.new(id: 101, service: Service.first, created_at: 1.hours.ago, resolved_at: 20.minutes.ago)
+    incident.components << Service.first.components.first
+    incident.hosts << Service.first.hosts.first
     IncidentMailer.resolved(incident)
   end
 end
